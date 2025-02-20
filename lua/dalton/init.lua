6601ee03a7b25d76b1157d718a23c03e841382d4
Dalton Avery <daltonjavery@gmail.com>
2025-02-20 11:07:39 -0600

    add nvim

---
+++ lua/dalton/init.lua
@@ -0,0 +1,7 @@
+require("dalton.remap")
+require("dalton.lazy_init")
+require("dalton.set")
+
+-- GRUVBOX CONFIG
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
---
+++ lua/dalton/lazy/lsp.lua
@@ -0,0 +1,109 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+    },
+
+    config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+
+                ["clangd"] = function ()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.clangd.setup {
+                        cmd = { "clangd",
+                                "--all-scopes-completion",
+                                "--background-index",
+                                "--clang-tidy",
+                                "--compile_args_from=filesystem",
+                                "--completion-parse=always",
+                                "--completion-style=bundled",
+                                "--cross-file-rename",
+                                "--debug-origin",
+                                "--enable-config",
+                                "--fallback-style=Qt",
+                                "--folding-ranges",
+                                "--function-arg-placeholders",
+                                "--header-insertion=iwyu",
+                                "--pch-storage=memory",
+                                "--suggest-missing-includes",
+                                "-j=4",
+                                "--log=error",
+                            },
+                        filetypes = { "c", "cpp", "objc", "objcpp" },
+                        single_file_support = true,
+                        capabilities = capabilities,
+                    }
+                end
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
+}
